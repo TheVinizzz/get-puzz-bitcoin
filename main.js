f85573313713b8fa2639297e65a9384f448bd182
Vinicius <vinipbgame@gmail.com>
2024-06-14 12:39:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+﻿import ranges from './ranges.js'
+import encontrarBitcoins, {encontrarBitcoinsRandom} from './bitcoin-find.js'
+import readline from 'readline'
+import chalk from 'chalk'
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let shouldStop = false;
+
+let key = 0;
+let min, max = 0;
+
+console.clear();
+
+console.log(process.env.TYPE_EXEC)
+
+console.log("\x1b[38;2;250;128;114m" + "╔════════════════════════════════════════════════════════╗\n" +
+            "║" + "\x1b[0m" + "\x1b[36m" + "   ____ _____ ____   _____ ___ _   _ ____  _____ ____   " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+            "║" + "\x1b[0m" + "\x1b[36m" + "  | __ )_   _/ ___| |  ___|_ _| \\ | |  _ \\| ____|  _ \\  " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+            "║" + "\x1b[0m" + "\x1b[36m" + "  |  _ \\ | || |     | |_   | ||  \\| | | | |  _| | |_) | " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+            "║" + "\x1b[0m" + "\x1b[36m" + "  | |_) || || |___  |  _|  | || |\\  | |_| | |___|  _ <  " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+            "║" + "\x1b[0m" + "\x1b[36m" + "  |____/ |_| \\____| |_|   |___|_| \\_|____/|_____|_| \\_\\ " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+            "║" + "\x1b[0m" + "\x1b[36m" + "                                                        " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+            "╚══════════════════════\x1b[32m" + "Investidor Internacional - v0.4" + "\x1b[0m\x1b[38;2;250;128;114m═══╝" + "\x1b[0m");
+
+
+if(process.env.TYPE_EXEC === "forever") {
+    let answer4 = process.env.RANGE
+    let key = process.env.HASH_CODE
+    let min = key
+    console.log(chalk.cyan("Rodando em forever"))
+    encontrarBitcoinsRandom(key, min, max, () => shouldStop, answer4)
+} else {
+    rl.question(`Escolha uma carteira puzzle( ${chalk.cyan(1)} - ${chalk.cyan(160)}): `, (answer) => {
+    
+        if (parseInt(answer) < 1 || parseInt(answer) > 160) {
+            console.log(chalk.bgRed('Erro: voce precisa escolher um numero entre 1 e 160'))
+        }
+    
+        min = ranges[answer-1].min
+        max = ranges[answer-1].max
+        console.log('Carteira escolhida: ', chalk.cyan(answer), ' Min: ', chalk.yellow(min), ' Max: ', chalk.yellow(max) )
+        console.log('Numero possivel de chaves:',  chalk.yellow(parseInt(BigInt(max) - BigInt(min)).toLocaleString('pt-BR')))
+        key = BigInt(min)
+        
+        rl.question(`Escolha uma opcao (${chalk.cyan(1)} - Comecar do inicio, ${chalk.cyan(2)} - Escolher uma porcentagem, ${chalk.cyan(3)} - Escolher minimo): `, (answer2) => {
+            if (answer2 == '2'){
+                rl.question('Escolha um numero entre 0 e 1: ', (answer3) => {
+                    if (parseFloat(answer3) > 1 || parseFloat(answer3) < 0) {
+                        console.log(chalk.bgRed('Erro: voce precisa escolher um numero entre 0 e 1'))
+                        throw 'Numero invalido'
+                    }
+    
+    
+                    const range = BigInt(max) - BigInt(min);
+                    const percentualRange = range * BigInt(Math.floor(parseFloat(answer3) * 1e18)) / BigInt(1e18);
+                    min = BigInt(min) + BigInt(percentualRange);
+                    console.log('Comecando em: ', chalk.yellow('0x'+min.toString(16)));
+                    key = BigInt(min)
+                    encontrarBitcoins(key, min, max, () => shouldStop)
+                    rl.close();
+                });
+            } else if (answer2 == '3'){
+                rl.question('Entre o minimo: ', (answer3) => {
+                    min = BigInt(answer3)
+                    key = BigInt(min)
+                    encontrarBitcoins(key, min, max, () => shouldStop)
+                    rl.close();
+                });
+            } else if (answer2 == '4'){
+                rl.question('Entre o minimo: ', (answer3) => {
+                    min = answer3
+                    key = answer3
+                    rl.question('Qual Range de caracteres: ', (answer4) => {
+                        encontrarBitcoinsRandom(key, min, max, () => shouldStop, answer4)
+                        rl.close();
+                    })
+                });
+            } else {
+                min = BigInt(min)
+                encontrarBitcoins(key, min, max, () => shouldStop)
+                rl.close();
+            }
+        })
+    });
+    
+    rl.on('SIGINT', () => {
+        shouldStop = true;
+        rl.close();
+        process.exit();
+    });
+    
+    process.on('SIGINT', () => {
+        shouldStop = true;
+        rl.close();
+        process.exit();
+    });
+}
+
